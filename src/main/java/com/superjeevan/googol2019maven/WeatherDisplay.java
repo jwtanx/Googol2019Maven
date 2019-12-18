d3e950df25e61748c9928fdd4f574f487cfc75cd
Google-X <i.code.like.crazy.everyday@gmail.com>
2019-12-19 01:26:33 +0800

    Googol2019 Weather specific day @ 191219, 01:30

--- src/main/java/com/superjeevan/googol2019maven/WeatherDisplay.java
+++ src/main/java/com/superjeevan/googol2019maven/WeatherDisplay.java
@@ -6,21 +6,26 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class WeatherDisplay {
 
-    private File filename = new File("WeatherData.dat");
+    private File filename = new File("WeatherData.txt");
     private Scanner s = new Scanner(System.in);
+    private DateFormat sd = new SimpleDateFormat("dd.MMM");
 
-    public WeatherDisplay() {
+    public WeatherDisplay(String cmd) {
         System.out.println("\nThis weather is provided by WeatherAtlas (https://www.weather-my.com)");
-        lookFile();
+        lookFile(cmd);
     }
 
-    public void lookFile() {
+    public void lookFile(String cmd) {
 
         Date d1 = new Date(filename.lastModified());
 
@@ -33,28 +38,101 @@ public class WeatherDisplay {
                 System.out.println("Could not update at the moment. Please connect to Internet to update.");
             }
         } else {
-            display();
+            
+            if (cmd.toLowerCase().contains("today")) {
+                
+                Date todayDate = new Date();
+                displayExactWeather(sd.format(todayDate));
+                
+            } else if (cmd.toLowerCase().contains("tomorrow")) {
+                
+                Date todayDate = new Date();
+
+                Calendar c = Calendar.getInstance();
+                //Setting the date to the given date
+                c.setTime(todayDate);
+
+                //Number of Days to add
+                c.add(Calendar.DAY_OF_MONTH, 1);
+                //Date after adding the days to the given date
+                Date tomorrow = c.getTime();
+
+                displayExactWeather(sd.format(tomorrow));
+                
+            } else if (cmd.toLowerCase().contains("yesterday")) {
+                
+                Date todayDate = new Date();
+
+                Calendar c = Calendar.getInstance();
+                //Setting the date to the given date
+                c.setTime(todayDate);
+
+                //Number of Days to add
+                c.add(Calendar.DAY_OF_MONTH, -1);
+                //Date after adding the days to the given date
+                Date yesterday = c.getTime();
+
+                displayExactWeather(sd.format(yesterday));
+                
+            } else if (cmd.contains("/")){
+                
+                cmd = cmd.toLowerCase();
+                cmd = cmd.replaceAll("/", "");
+                String findDate[] = cmd.split(" ");
+                Date dateToSearch = new Date();
+                String tempDate = "";
+                
+                for(int i = 0; i < findDate.length; i++){
+                    
+                    if(isDate(findDate[i])){
+                        
+                        if(findDate[i].substring(2, 4).equals("01")) tempDate = findDate[i].substring(0, 2) + ".JAN";
+                        if(findDate[i].substring(2, 4).equals("02")) tempDate = findDate[i].substring(0, 2) + ".FEB";
+                        if(findDate[i].substring(2, 4).equals("03")) tempDate = findDate[i].substring(0, 2) + ".MAR";
+                        if(findDate[i].substring(2, 4).equals("04")) tempDate = findDate[i].substring(0, 2) + ".APR";
+                        if(findDate[i].substring(2, 4).equals("05")) tempDate = findDate[i].substring(0, 2) + ".MAY";
+                        if(findDate[i].substring(2, 4).equals("06")) tempDate = findDate[i].substring(0, 2) + ".JUN";
+                        if(findDate[i].substring(2, 4).equals("07")) tempDate = findDate[i].substring(0, 2) + ".JUL";
+                        if(findDate[i].substring(2, 4).equals("08")) tempDate = findDate[i].substring(0, 2) + ".AUG";
+                        if(findDate[i].substring(2, 4).equals("09")) tempDate = findDate[i].substring(0, 2) + ".SEP";
+                        if(findDate[i].substring(2, 4).equals("10")) tempDate = findDate[i].substring(0, 2) + ".OCT";
+                        if(findDate[i].substring(2, 4).equals("11")) tempDate = findDate[i].substring(0, 2) + ".NOV";
+                        if(findDate[i].substring(2, 4).equals("12")) tempDate = findDate[i].substring(0, 2) + ".DEC";
+                        
+                        try{
+                            dateToSearch = sd.parse(tempDate);
+                        } catch (ParseException pe){
+                            System.out.println("Error parsing your date. Please try \"Show 30/12 weather\"");
+                        }
+                        break;
+                        
+                    }
+                }
+                
+                displayExactWeather(sd.format(dateToSearch));
+                
+            } else {
+                display();
+            }
+            
         }
     }
 
     public void display() {
         try {
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+            Scanner in = new Scanner(new FileInputStream(filename));
 
-            try {
-                System.out.println();
-                while (true) {
-                    System.out.println(in.readUTF());
-                }
-            } catch (EOFException EOF) {
-
+            for(int i = 0; i < 10; i++){
+                System.out.println(in.nextLine());
             }
+            
             in.close();
+        
         } catch (FileNotFoundException fnf) {
             System.err.println("Weather data not found. Update to check the weather.");
         } catch (IOException ie) {
             System.err.println("Error reading data. Please try again.");
-        }
+        } 
 
     }
 
@@ -82,5 +160,51 @@ public class WeatherDisplay {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) > 8;
 
     }
+    
+    public void displayExactWeather(String weatherStr) {
+        
+        try {
+            Date dayToSearch = sd.parse(weatherStr);
+            
+            Scanner sc = new Scanner(new FileInputStream(filename));
+            int line = 10;
+            
+            // Location
+            System.out.println(sc.nextLine());
+            
+            for(int i = 0; i < line; i++){
+                
+                String[] dateStr = sc.nextLine().split(" : ");
+                Date dateOnLine = sd.parse(dateStr[0].substring(4));
+                
+                if(dayToSearch.equals(dateOnLine)){
+                    System.out.println(dateStr[0] + " : " + dateStr[1]);
+                    break;
+                }
+                
+            }
 
+            sc.close();
+            
+            
+        } catch (FileNotFoundException fnf) {
+            System.err.println("File not found!");
+        } catch (ParseException pe) {
+
+        } catch (IOException ioe){
+            
+        }
+    }
+    public boolean isDate(String strNum) {
+
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
 }
